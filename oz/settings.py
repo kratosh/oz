214f891b76387247aa497a4d6ed40b520c81427a
Yonatan Perry <yonatan@mintigo.com>
2013-05-01 22:58:55 +0300

    static root fix

--- oz/settings.py
+++ oz/settings.py
@@ -62,7 +62,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(os.getcwd(), 'staticfiles')
+STATIC_ROOT = '/app/staticfiles'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
