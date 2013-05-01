d340416dda18aeee57ba304decff66bb9cf99418
Yonatan Perry <yonatan@mintigo.com>
2013-05-01 23:26:47 +0300

    static heroku, again

--- oz/settings.py
+++ oz/settings.py
@@ -1,5 +1,7 @@
 # Django settings for oz project.
-import os
+import os.path
+
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -62,7 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/app/staticfiles'
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'staticfiles')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
